fccd3bbe3f040d8c30ff59c2bebbe39379aec8c2
João Cenaque <joaocenaqueleite@hotmail.com>
2025-08-06 14:22:40 -0300

    fix: attribute names to works properly

--- src/main/resources/db/migration/V1__create-customer-table.sql
+++ src/main/resources/db/migration/V1__create-customer-table.sql
@@ -2,9 +2,9 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE customer (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    name VARCHAR(80) NOT NULL,
+    full_name VARCHAR(80) NOT NULL,
     email VARCHAR(320) NOT NULL,
     phone VARCHAR(16) NOT NULL,
     cpf VARCHAR(11) NOT NULL,
-    dateBirth DATE NOT NULL
+    date_birth DATE NOT NULL
 );
