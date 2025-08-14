64c2a511e4bd114fb4b918f23e1522524d7ff0cc
jot4sz <joaocenaqueleite@hotmail.com>
2025-08-13 23:54:20 -0300

    feat: add ProductResponseDTO

---
+++ src/main/java/com/ecomerce/api/domain/product/ProductResponseDTO.java
@@ -0,0 +1,15 @@
+package com.ecomerce.api.domain.product;
+
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record ProductResponseDTO(UUID id,
+                                 String name,
+                                 String brand,
+                                 String description,
+                                 BigDecimal price,
+                                 String technicalInfo,
+                                 Boolean available) {
+}
