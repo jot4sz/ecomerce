8115264d959c20145f03b60733ba477af4debae5
jot4sz <joaocenaqueleite@hotmail.com>
2025-08-13 23:59:03 -0300

    feat: add brand column and change technical_info to TEXT

--- src/main/resources/db/migration/V2__create-product-table.sql
+++ src/main/resources/db/migration/V2__create-product-table.sql
@@ -1,8 +1,9 @@
 CREATE TABLE product (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
+    brand VARCHAR(50) NOT NULL,
     description VARCHAR(1000) NOT NULL,
     price NUMERIC(8,2) NOT NULL,
-    technical_info VARCHAR(1000),
+    technical_info TEXT NOT NULL,
     available BOOLEAN NOT NULL
 );
