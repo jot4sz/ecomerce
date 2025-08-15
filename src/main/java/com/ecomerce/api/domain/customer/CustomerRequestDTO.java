16a48ee4410024f10fa60690e795f3f9b453c4b0
jot4sz <joaocenaqueleite@hotmail.com>
2025-08-15 14:32:21 -0300

    feat: add the first Annotations on CustomerRequestDTO

--- src/main/java/com/ecomerce/api/domain/customer/CustomerRequestDTO.java
+++ src/main/java/com/ecomerce/api/domain/customer/CustomerRequestDTO.java
@@ -1,10 +1,22 @@
 package com.ecomerce.api.domain.customer;
 
 
-public record CustomerRequestDTO( String fullName,
-                                  String email,
-                                  String phone,
-                                  String cpf,
-                                  String dateBirth
+import com.ecomerce.api.validation.CPF;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Date;
+
+public record CustomerRequestDTO(@NotBlank
+                                 String fullName,
+                                 @Email(message = "Invalid email format")
+                                 String email,
+                                 @NotBlank
+                                 String phone,
+                                 @CPF
+                                 String cpf,
+                                 @JsonFormat(pattern = "dd/MM/yyyy")
+                                 Date dateBirth
 ) {
 }
