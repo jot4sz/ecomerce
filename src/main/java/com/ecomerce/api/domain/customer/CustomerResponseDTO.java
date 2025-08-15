ad08fb46f8d6e283349bd6591ba3b1fe7578ffe8
jot4sz <joaocenaqueleite@hotmail.com>
2025-08-15 14:32:54 -0300

    feat: add CustomerResponseDTO

---
+++ src/main/java/com/ecomerce/api/domain/customer/CustomerResponseDTO.java
@@ -0,0 +1,12 @@
+package com.ecomerce.api.domain.customer;
+
+import java.util.Date;
+import java.util.UUID;
+
+public record CustomerResponseDTO(UUID id,
+                                  String fullName,
+                                  String email,
+                                  String phone,
+                                  String cpf,
+                                  Date dateBirth) {
+}
