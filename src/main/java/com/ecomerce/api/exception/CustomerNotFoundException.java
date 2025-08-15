bf2cd84d13f7a11d84bb390746608f42370ea69b
jot4sz <joaocenaqueleite@hotmail.com>
2025-08-15 14:31:12 -0300

    feat: add CustomerNotFoundException

---
+++ src/main/java/com/ecomerce/api/exception/CustomerNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ecomerce.api.exception;
+
+public class CustomerNotFoundException extends RuntimeException {
+
+  public CustomerNotFoundException() { super("Customer not found");
+  }
+
+  public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
