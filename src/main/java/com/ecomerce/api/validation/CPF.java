5420480ab53803e04eab1d1a60430f25ef6c2a8f
jot4sz <joaocenaqueleite@hotmail.com>
2025-08-15 14:30:31 -0300

    feat: add CPF validation using Annotation

---
+++ src/main/java/com/ecomerce/api/validation/CPF.java
@@ -0,0 +1,18 @@
+package com.ecomerce.api.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = CpfValidator.class)
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CPF {
+    String message() default "CPF inválido";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
