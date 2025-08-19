0b5b433e002da41ba517d718472ba5c855e07c57
jot4sz <joaocenaqueleite@hotmail.com>
2025-08-19 20:25:59 -0300

    feat: add methods findByCustomerId and findByIdAndCustomerId

--- src/main/java/com/ecomerce/api/repositories/AddressRepository.java
+++ src/main/java/com/ecomerce/api/repositories/AddressRepository.java
@@ -3,7 +3,12 @@ package com.ecomerce.api.repositories;
 import com.ecomerce.api.domain.address.AddressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AddressRepository extends JpaRepository<AddressEntity, UUID> {
+    List<AddressEntity> findByCustomerId(UUID customerId);
+
+    Optional<AddressEntity> findByIdAndCustomerId(UUID addressId, UUID customerId);
 }
