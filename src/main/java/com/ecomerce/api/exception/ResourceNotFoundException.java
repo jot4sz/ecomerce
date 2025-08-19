3954af4a8123b158f8da01f9b57e1d5d25b8bea9
jot4sz <joaocenaqueleite@hotmail.com>
2025-08-19 20:26:53 -0300

    feat: create ResourceNotFoundException

---
+++ src/main/java/com/ecomerce/api/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ecomerce.api.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
