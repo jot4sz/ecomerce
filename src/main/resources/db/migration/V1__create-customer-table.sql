d06519fc7325b1cbe5e73558944cbe1e9e1575ed
jot4sz <joaocenaqueleite@hotmail.com>
2025-08-19 16:14:08 -0300

    add unique on email

--- src/main/resources/db/migration/V1__create-customer-table.sql
+++ src/main/resources/db/migration/V1__create-customer-table.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 CREATE TABLE customer (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     full_name VARCHAR(80) NOT NULL,
-    email VARCHAR(320) NOT NULL,
+    email VARCHAR(320) UNIQUE NOT NULL,
     phone VARCHAR(16) NOT NULL,
     cpf VARCHAR(11) UNIQUE NOT NULL,
     date_birth DATE NOT NULL
